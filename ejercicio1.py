1d17dbaae8ef88cec54274f823a6d15aaa5144c7
JulianaMaxit <julianamaxit@gmail.com>
2024-05-24 20:49:57 -0300

    Ejercicio 1 compleo

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+def maximo_comun_divisor(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+numero1 = 20
+numero2 = 4
+
+resultado = maximo_comun_divisor(numero1, numero2)
+
+print(f'El máximo común divisor de {numero1} y {numero2} es {resultado}')
